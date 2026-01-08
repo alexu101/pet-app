af9ae352a4e54c0af79be207cc73aa482f345f9d
alexu101 <alextanase60@yahoo.com>
2026-01-08 21:44:34 +0200

    prisma configured properly

--- server/src/config/config.db.ts
+++ server/src/config/config.db.ts
@@ -1,6 +1,17 @@
 import dotenv from 'dotenv'
-import {PrismaClient} from "../generated/prisma/client.js"
+import { PrismaClient } from '../generated/client.js'
 
 dotenv.config()
 
-export const prisma = new PrismaClient()
+export const prisma = new PrismaClient({
+    accelerateUrl: ''
+})
+
+export const connectToDb = async () => {
+    try {
+        await prisma.$connect()
+        console.log("Successfully connected to the database")
+    } catch(err) {
+        console.log("Connection to the database failed")
+    }
+}
